9b027ee536223f493a6a5e3a00a70bef2e02a7cf
maq10689 <108466005+maq10689@users.noreply.github.com>
2022-07-04 15:10:29 +0100

    HWv1

---
+++ Index1.js
@@ -0,0 +1,15 @@
+let spaghetti; 
+spaghetti = 400;
+let guanciale;
+guanciale = 250;
+let eggyolks; 
+eggyolks = 6;
+let cheese;
+cheese = 50; 
+let blackpepper;
+blackpepper = 4; 
+let carbanora;
+carbanora = spaghetti + guanciale + eggyolks + cheese + blackpepper;
+
+console.log("Total Ingredients Weight" + " " + (carbanora) + "g");
+
---
+++ index.js
@@ -0,0 +1,14 @@
+let firstname = "Maqaddas";
+let lastname = "Ali";
+let middlename = "Ansar";
+
+console.log("Hello my name is " + (firstname) + " " + (middlename) + " " + (lastname)); 
+
+let age = 27;
+let currentyear = 2022;
+
+let myage;
+myage = currentyear - age; 
+
+console.log("My Birth Year is: " + (myage)); 
+
